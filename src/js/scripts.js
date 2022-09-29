c118b98fd7e89c561bd0d1d67dd6010fc5abc4bb
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2022-09-29 18:51:45 +0300

    with physics & shadow

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as CANNON from "cannon-es";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // Sets the color of the background
@@ -16,6 +18,15 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(0, 50, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+scene.add(directionalLight);
+
+const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
+
 // Sets orbit control to move the camera around
 const orbit = new OrbitControls(camera, renderer.domElement);
 
@@ -27,20 +38,51 @@ orbit.update();
 const axesHelper = new THREE.AxesHelper(4);
 scene.add(axesHelper);
 
+const sphereBodyMat = new CANNON.Material();
+
 const createSphere = () => {
   const sphereGeo = new THREE.SphereGeometry(0.4, 20, 20);
-  const sphereMat = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
+  const sphereMat = new THREE.MeshStandardMaterial({
+    color: Math.random() * 0xffffff,
   });
   const sphere = new THREE.Mesh(sphereGeo, sphereMat);
 
-  return sphere;
+  const sphereBody = new CANNON.Body({
+    shape: new CANNON.Sphere(0.4),
+    mass: 1,
+    material: sphereMat,
+  });
+
+  return { mesh: sphere, body: sphereBody };
 };
 
-scene.background = new THREE.Color(0x333333);
+const planeGeo = new THREE.PlaneGeometry(20, 20, 20);
+const planeMat = new THREE.MeshStandardMaterial({
+  color: 0xccfeff,
+});
+const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+planeMesh.receiveShadow = true;
 
-const sphere = createSphere();
-scene.add(sphere);
+scene.background = new THREE.Color(0x333333);
+scene.add(planeMesh);
+
+const planeBodyMat = new CANNON.Material();
+
+const planeBody = new CANNON.Body({
+  shape: new CANNON.Box(new CANNON.Vec3(10, 10, 0.001)),
+  type: CANNON.Body.STATIC,
+  material: planeBodyMat,
+});
+planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(planeBody);
+
+const contactPlaneSphereMat = new CANNON.ContactMaterial(
+  sphereBodyMat,
+  planeBodyMat,
+  { restitution: 0.25 }
+);
+
+world.addContactMaterial(contactPlaneSphereMat);
 
 const raycaster = new THREE.Raycaster();
 const mousePosition = new THREE.Vector2();
@@ -57,13 +99,33 @@ document.addEventListener("mousemove", (e) => {
   raycaster.ray.intersectPlane(plane, intersectionPoint);
 });
 
+const meshes = [];
+
 document.addEventListener("click", (e) => {
   const sphere = createSphere();
-  scene.add(sphere);
-  sphere.position.copy(intersectionPoint);
+  scene.add(sphere.mesh);
+  sphere.mesh.castShadow = true;
+  sphere.mesh.receiveShadow = true;
+  world.addBody(sphere.body);
+
+  sphere.body.position = new CANNON.Vec3(
+    intersectionPoint.x,
+    intersectionPoint.y,
+    intersectionPoint.z
+  );
+
+  meshes.push(sphere);
 });
 
+const step = 1 / 60;
 function animate() {
+  world.step(step);
+  planeMesh.position.copy(planeBody.position);
+  planeMesh.quaternion.copy(planeBody.quaternion);
+  for (const msh of meshes) {
+    msh.mesh.position.copy(msh.body.position);
+    msh.mesh.quaternion.copy(msh.body.quaternion);
+  }
   renderer.render(scene, camera);
 }
 
